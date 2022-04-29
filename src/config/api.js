6d9ef2421971f7fa0b0cb6695124d81cc1dfec53
Heladio Arteaga Franco <harteagaf@atencion.com>
2022-04-28 23:59:10 -0500

    Changing env var

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 const apiUrl = 'https://api.nasa.gov/planetary/apod';
-const apiKey = process.env.NASA_KEY || '<PUT_HERE_YOUR_KEYS>';
+const apiKey = process.env.REACT_APP_NASA_KEY || '';
 const baseURL = `${apiUrl}?api_key=${apiKey}`;
 
 export { baseURL }
